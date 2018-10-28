0e4cf67c35bd4528004676c4c9b78265c56e329c
Alex Rossi Ignez <rx.rossi@gmail.com>
2018-10-28 16:15:15 +0000

    Add EntryView

---
+++ src/EntryView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EntryView({ time, AET }) {
+  return (
+    <li>
+      {time} {AET}
+    </li>
+  );
+}
+
+export default EntryView;
---
+++ src/EntryView.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import EntryView from "./EntryView";
+
+describe("EntryView", () => {
+  it("renders the date in expected format", () => {
+    const time = "some time";
+    const text = shallow(<EntryView time={time} AET={8} />).text();
+
+    expect(text).toMatch(time);
+    expect(text).toMatch("8");
+  });
+});
